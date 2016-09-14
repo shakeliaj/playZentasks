2e1f8f4b42e8a535c9ce0838e3d4b51d99b414bc
Shakelia <sjohnson@inginestudio.com>
2016-09-14 18:09:08 -0400

    working on the test cases for the login screen --> authenticateSuccess() and authenticateFailure()

--- test/controllers/LoginTest.java
+++ test/controllers/LoginTest.java
@@ -10,12 +10,19 @@ import play.mvc.*;
 import play.libs.*;
 import play.test.*;
 import static play.test.Helpers.*;
+import play.data.FormFactory;
+import play.data.Form;
 
 import com.avaje.ebean.Ebean;
 import com.google.common.collect.ImmutableMap;
+import controllers.HomeController.Login;
+import javax.inject.*;
 
 
 public class LoginTest extends WithApplication{
+	@Inject
+	FormFactory formFactory;
+	
 	@Before
 	public void setUp(){
 		start(fakeApplication(inMemoryDatabase()));
@@ -36,13 +43,28 @@ public class LoginTest extends WithApplication{
 			//save tasks
 			Ebean.save(task);
 		}
+		
 	}
 	@Test
 	public void authenticateSuccess(){
-		Result result = callAction(controllers.routes.ref.HomeController.authenticate(),
-				fakeRequest().withFormUrlEncodedBody(ImmutableMap.of("email","bob@example.com","password","secret"))
+		Form<HomeController.Login> info = new Form<HomeController.Login>();
+		info.put("email","bob@example.com");
+		info.put("password","secret");
+		Result result = callAction(
+			controllers.routes.HomeController.authenticate(),
+			fakeRequest().withFormUrlEncodedBody(formFactory.form(Login.class).bind(info))
 		);
 		assertEquals(302, result.status());
 		assertEquals("bob@example.com",result.session().get("email"));
+	}
+	@Test
+	public void authenticateFailure(){
+		Result result = callAction(
+			controllers.routes.HomeController.authenticate(),
+			fakeRequest().withFormUrlBodyEncodedBody(ImmutableMap.of("email","bob@example.com","password","badpassword"))
+		);
+		assertEquals(400,result.status());
+		assertNull(result.session().get("email"));
+		
 	}
 }
